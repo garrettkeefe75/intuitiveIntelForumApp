cf33b2e23fe7da399942e5ffae680094908929b2
garrettkeefe75 <57542053+garrettkeefe75@users.noreply.github.com>
2022-08-30 12:12:32 -0600

    Connected React App with postgres DB

--- chat-app-gametips/server/db.js
+++ chat-app-gametips/server/db.js
@@ -1,10 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "nandu",
-  port: 4000,
-  host: "localhost",
-  database: "forumapp",
+  user: "anyone",
+  port: 5432,
+  password: "teamgametips",
+  host: "database-1.cgpwhtgqxogz.us-west-1.rds.amazonaws.com",
+  database: "teamgametipsdb",
 });
 
 module.exports = pool;
