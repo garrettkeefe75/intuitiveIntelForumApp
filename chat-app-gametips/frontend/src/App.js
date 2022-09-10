a2e248dc6470ee49ef4042bcfb41e93f00d01525
nandakumar1200 <nandujonnalagadda@gmail.com>
2022-09-10 14:24:19 +0530

    created a basic comments template using material - UI, need to work on backend and routes now.

--- chat-app-gametips/frontend/src/App.js
+++ chat-app-gametips/frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //components
 import SignIn from "./components/LoginForm";
 import ThreadListingPage from "./components/ThreadsLanding";
+import Comments from "./components/CommentsSection";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/chatRooms" element={<ThreadListingPage />} />
+        <Route path="/Comments" element={<Comments />} />
       </Routes>
     </Router>
   );
