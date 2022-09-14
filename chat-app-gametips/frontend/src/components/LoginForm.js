9514d930cb5a73e867a8a870ab1c614494cd9b8d
garrettkeefe75 <57542053+garrettkeefe75@users.noreply.github.com>
2022-09-14 15:01:47 -0600

    Finishing touches for website google login

--- chat-app-gametips/frontend/src/components/LoginForm.js
+++ chat-app-gametips/frontend/src/components/LoginForm.js
@@ -1,63 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { gapi } from 'gapi-script';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Typography, Link } from '@mui/material';
+import { Stack } from '@mui/system';
+import { Fragment } from 'react';
+import ButtonAppBar from "./AppBar";
 
 export default function SignIn() {
-  const [profile, setProfile] = useState([]);
-  const clientId = '92924635618-jfp2hmchkrqlc6at7iakf4180jb4aeas.apps.googleusercontent.com';
-  const navigate = useNavigate();
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ''
-      });
-    };
-    gapi.load('client:auth2', initClient);
-  });
-
-  const onSuccess = (res) => {
-    setProfile(res.profileObj);
-    localStorage.setItem('user', profile);
-    navigate('/chatRooms');
-  };
-
-  const onFailure = (err) => {
-    console.log('failed', err);
-  };
-
-  const logOut = () => {
-    setProfile(null);
-    localStorage.clear();
-  };
-
   return (
-    <div>
-      <h2>React Google Login</h2>
-      <br />
-      <br />
-      {profile ? (
-        <div>
-          <img src={profile.imageUrl} alt="user" />
-          <h3>User Logged in</h3>
-          <p>Name: {profile.name}</p>
-          <p>Email Address: {profile.email}</p>
-          <p>Google ID: {profile.googleId}</p>
-          <br />
-          <br />
-          <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
-        </div>
-      ) : (
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Sign in with Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={'single_host_origin'}
-          isSignedIn={true}
-        />
-      )}
-    </div>
+    <Fragment>
+      <Stack spacing={3}>
+        <ButtonAppBar />
+        <Typography variant="h2" component="div" align="center" color="blue">
+          Welcome to Intuitive Intel Forum! 
+        </Typography>
+        <Link href='/chatRooms' align='center'>Click here to see our available chat rooms!</Link>
+      </Stack>
+    </Fragment>
   );
 }
