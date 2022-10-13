bb00463b197e6cd4336091f70c1e8739d7c77833
garrettkeefe75 <57542053+garrettkeefe75@users.noreply.github.com>
2022-10-13 02:08:01 -0600

    Provided a basic view of users tips

--- chat-app-gametips/frontend/src/components/TipCardComponent.js
+++ chat-app-gametips/frontend/src/components/TipCardComponent.js
@@ -9,8 +9,11 @@ import {
   CardHeader,
   IconButton,
 } from "@material-ui/core";
+import React, { useState, useEffect, Fragment } from "react";
+import { Stack } from "@mui/material";
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ButtonAppBar from "./AppBar";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -25,27 +28,78 @@ const useStyles = makeStyles((theme) =>
 );
 
 export default function TipsComponent() {
+  const [profile, setProfile] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [tips, setTips] = useState([]);
   const classes = useStyles();
+
+  useEffect(() => {
+    const loggedInUser = JSON.parse(window.localStorage.getItem("user"));
+    if (loggedInUser) {
+      setProfile(loggedInUser);
+      setLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const getTips = async (user_id) => {
+      try {
+        console.log(user_id);
+        console.log(String(user_id));
+        const response = await fetch("http://localhost:5000/tips/".concat(String(user_id)));
+        const jsonData = await response.json();
+        setTips(jsonData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    console.log(profile);
+    if(profile !== [])
+      getTips(profile.user_id);
+  }, [profile]);
+
+
+
+  if (!loggedIn) {
+    return (
+      <Fragment>
+        <Stack spacing={3}>
+          <ButtonAppBar />
+          <Typography variant="h2" component="div" align="center" color="primary">
+            No tips to display, try logging in.
+          </Typography>
+        </Stack>
+      </Fragment>
+
+    )
+  }
   return (
-    <Box className={classes.root}>
-      <Card>
-        <CardHeader
-          avatar={<Avatar>N</Avatar>}
-          title="Card Header"
-          subheader="Card sub heading"
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-        />
-        <CardContent>
-          <Typography variant="h3">Card Title</Typography>
-        </CardContent>
-        <CardActions>
-          <Button>share</Button>
-        </CardActions>
-      </Card>
-    </Box>
+    <Fragment>
+      <Stack spacing={3}>
+        <ButtonAppBar />
+        <Box className={classes.root}>
+          {tips.map((tip) => (
+            <Card key={tip.tipid}>
+              <CardHeader
+                avatar={<Avatar>N</Avatar>}
+                title={tip.title}
+                subheader="Card sub heading"
+                action={
+                  <IconButton>
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+              />
+              <CardContent>
+                <Typography variant="h3">{tip.explanation}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button>share</Button>
+              </CardActions>
+            </Card>
+          ))}
+        </Box>
+      </Stack>
+    </Fragment>
   );
 }
